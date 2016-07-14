2c73f66ae18a9c072729ebaed49e266dc2f3816e
Fuegan <albertogasco@gmail.com>
2016-07-14 23:13:32 +0200

    Update auditor.py

--- auditor.py
+++ auditor.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 
 import os
+import time
 #import nmap
-print("Bienvenido al creador de estructuras de carpetas para auditoria v 0.1")
+
+
+#Introducir informacion basica sobre la auditoria
+print("Bienvenido al creador de estructuras de carpetas para auditoria v 0.2")
 empresa = input('Por favor introduzca el nombre de la empresa a auditar:')
 auditor=input('Por favor introduzca el nombre del auditor asignado:')
 #rango=input('Por favor introduzca la subred a auditar (Direccion ip/mascara de red):')
+
+#Creacion de carpetas donde contener la informacion
 ruta=os.environ["HOME"]+str("/Auditoria/")+str(empresa)
 os.makedirs(ruta+"/imagenes_videos/Escaneo")
 os.makedirs(ruta+"/imagenes_videos/Reconocimiento")
 os.makedirs(ruta+"/imagenes_videos/Analisis_vulnerabilidades")
-os.makedirs(ruta+"/imagenes_videos/Explotacion")
+os.makedirs(ruta+"/imagenes_videos/Explotación")
 os.makedirs(ruta+"/Reporte_aplicaciones")
 os.makedirs(ruta+"/Data")
-fb=str(ruta)+str('bitacora.txt')
+#Creacion de archivo de bitacora
+fb=str(ruta)+str('/bitacora.txt')
 bitacora=open( fb , 'w')
-bitacora.write('Auditoria:\nEmpresa: '+ empresa + '\nAuditor asignado: '+auditor +  '\nBitacora dia 1:\n')
+bitacora.write('Auditoria:\nEmpresa: '+ empresa + '\nAuditor asignado: '+auditor +  '\nBitacora:\n')
+bitacora.close()
+bitacora=open( fb , 'a')
+ahora = str(time.strftime('%y %b %d') + " - " + time.strftime('%H:%M'))
+bitacora.write(ahora)
 bitacora.close()
